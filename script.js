49f404897bf30f4610dd42614f06bbc0bb1f0197
jesus villegas <jesusvicajr@hotmail.com>
2022-08-16 12:06:49 -0700

    added_localStorage

--- script.js
+++ script.js
@@ -31,4 +31,35 @@ if (todo != null){
     }
 }
 
+//function to save in local storage
+
+var saveBtn = document.querySelector(".saveBtn")
+//onclick button to save the written text to local storage
+$('.saveBtn').click(function(event){
+    var id = $(this).parent().attr("id");
+    var textA = $("tr .9").children().eq(1).children().val();
+    var textarea = $(this).siblings('td').eq(0).children().val();
+    console.log(id);
+    console.log(textarea);
+
+
+    //checks for local stoage
+    var todo = []; 
+    var checkStorage = JSON.parse(localStorage.getItem("todoList"));
+    if (checkStorage) {
+        for (var i = 0; i < checkStorage.length; i++){
+            todo.push(checkStorage[i]);
+        }
+        //adds to local storage
+        localStorage.setItem("todoList", JSON.stringify(todo));
+    }
+
+    //adds the new todo item to the todo list
+    todo[todo.length] = {
+        timeBlock: id,
+        todoItem: textarea
+    }
+    localStorage.setItem("todoList", JSON.stringify(todo));
+
+}); 
 
